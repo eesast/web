7d14e72c34f5c84c5078ea8751ced952491b109c
Robert Ying <yingrui205@gmail.com>
2019-07-01 09:57:49 +0800

    Setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "antd";
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 const App: React.FC = () => {
   return (
@@ -10,17 +11,10 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>Learn React</Button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
