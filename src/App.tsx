be718028c0d98c67ab7ea94a36bd88e2b675bae0
Robert Ying <yingrui205@gmail.com>
2019-07-03 18:37:12 -0700

    Convert App to class implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import React from "react";
 import "./App.css";
 import logo from "./logo.svg";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button>Learn React</Button>
-      </header>
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <Button>Learn React</Button>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
