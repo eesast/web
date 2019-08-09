d929c2235bea2b39e036786d0e8ccc8057bd6ef4
Rui Ying <yingrui205@gmail.com>
2019-08-09 12:08:09 -0700

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,7 +8,6 @@ axios.defaults.baseURL =
   process.env.NODE_ENV === "production"
     ? "https://api.eesast.com"
     : "http://localhost:28888";
-//axios.defaults.baseURL = "https://api.eesast.com";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 export default {
