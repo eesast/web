f9072458795038257af3b3fea5156efe232cd16f
Robert Ying <yingrui205@gmail.com>
2019-08-12 18:34:54 -0700

    Add auth header once logged in

--- src/redux/reducers/auth.ts
+++ src/redux/reducers/auth.ts
@@ -3,9 +3,11 @@ import { IAuthAction } from "../types/actions";
 import {
   LOGIN_FAILURE,
   LOGIN_REQUEST,
-  LOGIN_SUCCESS
+  LOGIN_SUCCESS,
+  UPDATE_USER
 } from "../types/constants";
 import { IAuthState } from "../types/state";
+import axios from "axios";
 
 export default function auth(
   state: IAuthState = {
@@ -16,6 +18,7 @@ export default function auth(
 ): IAuthState {
   switch (action.type) {
     case LOGIN_REQUEST:
+      axios.defaults.headers.common["Authorization"] = "";
       return {
         ...state,
         loggingIn: true,
@@ -24,6 +27,7 @@ export default function auth(
     case LOGIN_SUCCESS:
       const token = action.payload;
       const decoded = jwtDecode(token) as any;
+      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
       return {
         ...state,
         token,
@@ -33,11 +37,21 @@ export default function auth(
         user: decoded
       };
     case LOGIN_FAILURE:
+      axios.defaults.headers.common["Authorization"] = "";
       return {
         ...state,
         loggedIn: false,
         loggingIn: false,
         error: action.payload
+      };
+    case UPDATE_USER:
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...action.payload.user,
+          id: action.payload.id
+        }
       };
   }
   return state;
