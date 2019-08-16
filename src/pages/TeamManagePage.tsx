f2476642670d7ccf1af4685566191fc009e00fea
zxdclyz <1515685918@qq.com>
2019-08-16 23:25:12 +0800

    Add TeamManageForm

--- src/pages/TeamManagePage.tsx
+++ src/pages/TeamManagePage.tsx
@@ -2,16 +2,15 @@ import React, { useEffect } from "react";
 import { Redirect, withRouter } from "react-router-dom";
 import { IAppState, ITeam } from "../redux/types/state";
 import { connect } from "react-redux";
-// import { FormComponentProps } from "antd/lib/form";
+import { FormComponentProps } from "antd/lib/form";
 import { getTeams, getContestId } from "../redux/actions/teams";
-import { message, Card, Descriptions } from "antd";
+import { message, Card, Form, Input, Icon, Row, Button } from "antd";
 import { WithRouterComponent } from "../types/WithRouterComponent";
 import styles from "./TeamManagePage.module.css";
 
 interface ITeamManagePageStateProps {
   loggedIn: boolean;
   teams: ITeam[];
-  fetching: boolean;
   token?: string;
   contestId?: number;
   error?: Error | null;
@@ -28,7 +27,7 @@ type ITeamManagePageProps = ITeamManagePageDispatchProps &
 const TeamManagePage: React.FC<
   WithRouterComponent<{}, ITeamManagePageProps>
 > = props => {
-  const { loggedIn, teams, fetching, error, getTeams } = props;
+  const { loggedIn, teams, token, contestId, error, getTeams } = props;
 
   useEffect(() => {
     getTeams(true, "电设", 2019);
@@ -45,30 +44,14 @@ const TeamManagePage: React.FC<
     if (!teams.length) {
       return <Redirect to={"/thuedc/teams/join"} />;
     } else {
-      const {
-        name,
-        description,
-        inviteCode,
-        leaderUsername,
-        membersUsername = []
-      } = teams[0];
-
       return (
         <div className={styles.root}>
-          <Card className={styles.card} loading={fetching}>
-            <Descriptions title="队伍信息" column={4}>
-              <Descriptions.Item label="队名">{name}</Descriptions.Item>
-              <Descriptions.Item label="邀请码">{inviteCode}</Descriptions.Item>
-              <Descriptions.Item label="队长">
-                {leaderUsername}
-              </Descriptions.Item>
-              <Descriptions.Item label="队员">
-                {membersUsername!.join(", ")}
-              </Descriptions.Item>
-              <Descriptions.Item label="队伍简介">
-                {description}
-              </Descriptions.Item>
-            </Descriptions>
+          <Card className={styles.card}>
+            <WrappedTeamManageForm
+              token={token || ""}
+              contestId={contestId}
+              props={props}
+            />
           </Card>
         </div>
       );
@@ -82,7 +65,6 @@ function mapStateToProps(state: IAppState): ITeamManagePageStateProps {
   return {
     loggedIn: state.auth.loggedIn,
     teams: state.teams.items,
-    fetching: state.teams.fetching,
     token: state.auth.token,
     contestId: state.teams.contestId,
     error: state.teams.error
@@ -101,8 +83,78 @@ export default withRouter(
   )(TeamManagePage)
 );
 
-// interface ITeamManageFormProps extends FormComponentProps {
-//   props: WithRouterComponent<{}, ITeamManagePageProps>;
-//   token: string;
-//   contestId?: number;
-// }
+interface ITeamManageFormProps extends FormComponentProps {
+  props: WithRouterComponent<{}, ITeamManagePageProps>;
+  token: string;
+  contestId?: number;
+}
+
+const TeamManageForm: React.FC<ITeamManageFormProps> = ({
+  form,
+  props,
+  token,
+  contestId
+}) => {
+  const {
+    name,
+    description,
+    inviteCode,
+    leaderUsername,
+    membersUsername = []
+  } = props.teams[0];
+
+  const { getFieldDecorator } = form;
+
+  const formItemLayout = {
+    labelCol: { span: 6 },
+    wrapperCol: { span: 14 }
+  };
+
+  return (
+    <Form {...formItemLayout}>
+      <Form.Item label="队伍名称">
+        {getFieldDecorator("name", {
+          rules: [{ required: true, message: "请输入队伍名称!" }],
+          initialValue: name
+        })(
+          <Input
+            prefix={<Icon type="team" style={{ color: "rgba(0,0,0,.25)" }} />}
+            style={{ width: "30%" }}
+            autoCapitalize="off"
+            autoCorrect="off"
+            autoComplete="on"
+          />
+        )}
+      </Form.Item>
+      <Row>
+        <Form.Item label="邀请码">
+          <span>{inviteCode}</span>
+        </Form.Item>
+        <Form.Item label="队长">
+          <span>{leaderUsername}</span>
+        </Form.Item>
+      </Row>
+      <Form.Item label="队员">
+        <span>{membersUsername!.join(", ")}</span>
+      </Form.Item>
+      <Form.Item label="队伍简介">
+        {getFieldDecorator("description", {
+          rules: [{ required: true, message: "请输入队伍简介!" }],
+          initialValue: description
+        })(
+          <Input.TextArea
+            autosize={{ minRows: 5, maxRows: 10 }}
+            autoComplete="on"
+          />
+        )}
+      </Form.Item>
+      <Form.Item style={{ textAlign: "center" }}>
+        <Button type="primary">确认修改</Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+const WrappedTeamManageForm = Form.create<ITeamManageFormProps>()(
+  TeamManageForm
+);
