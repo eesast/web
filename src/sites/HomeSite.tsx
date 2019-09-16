768a73c07d24bbe1cccbab7e12b3eaeb9f825881
Robert Ying <yingrui205@gmail.com>
2019-09-16 13:24:03 +0800

    Adjust styles & upload photos

--- src/sites/HomeSite.tsx
+++ src/sites/HomeSite.tsx
@@ -1,67 +1,48 @@
 import { Layout, Menu, Icon } from "antd";
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Site } from "../App";
 import FeedTimeline from "../pages/FeedTimeline";
 import styles from "./HomeSite.module.css";
 import logo from "../assets/logo.png";
 import { Typography } from "antd";
-import { ClickParam } from "antd/lib/menu";
-import BranchesPage from "../pages/BranchesPage";
-const { useState } = React;
-const { Header } = Layout;
+import { MenuProps } from "antd/lib/menu";
+import DivisionPage from "../pages/DivisionPage";
+import { Link, Switch, Route } from "react-router-dom";
+import { WithRouterComponent } from "../types/WithRouterComponent";
+import NotFoundSite from "./NotFoundSite";
+
+const { Header, Content } = Layout;
 
 export interface IHomeSiteProps {
   setSite: (site: Site) => void;
 }
-type homepageOption = "timelines" | "branches" | "games";
 
-const HomepageOption: (option: string) => homepageOption = option => {
-  switch (option) {
-    case "timelines":
-      return "timelines";
-    case "branches":
-      return "branches";
-    case "games":
-      return "games";
-    default:
-      return "timelines";
-  }
-};
-export interface IHomepageSelections {
-  selected: homepageOption;
-}
+type HomeSiteTab = "timelines" | "divisions" | "contests";
 
-const HomepageOptions: React.FC<IHomepageSelections> = selection => {
-  const option = selection.selected;
-  switch (option) {
-    case "timelines":
-      return <FeedTimeline />;
-    case "branches":
-      return <BranchesPage />;
-    default:
-      return (
-        <div className={styles.root}>
-          <img className={styles.logo} alt="logo" src={logo} />
-          <Typography.Title level={3}>官网建设中...</Typography.Title>
-        </div>
-      );
-  }
-};
-
-const HomeSite: React.FC<IHomeSiteProps> = ({ setSite }) => {
+const HomeSite: React.FC<WithRouterComponent<{}, IHomeSiteProps>> = ({
+  setSite,
+  match
+}) => {
   setSite("home");
-  const [currentSelect, setCurrentSelect] = useState<homepageOption>(
-    "branches"
+
+  const NotFoundPage = useCallback(() => <NotFoundSite setSite={setSite} />, [
+    setSite
+  ]);
+
+  const [currentSelected, setCurrentSelect] = useState<HomeSiteTab>(
+    match.path === "/home"
+      ? "timelines"
+      : match.path === "/home/divisions"
+      ? "divisions"
+      : "contests"
   );
-  const clickHandler = (e: ClickParam) => {
-    setCurrentSelect(HomepageOption(e.key));
+
+  const handleMenuSelect: MenuProps["onSelect"] = e => {
+    setCurrentSelect(e.key as HomeSiteTab);
   };
+
   return (
-    <Layout
-      style={{
-        height: "calc(100vh - 64px)"
-      }}
-    >
+    <Layout>
       <Header
         style={{
           backgroundColor: "#fff",
@@ -73,26 +54,58 @@ const HomeSite: React.FC<IHomeSiteProps> = ({ setSite }) => {
         }}
       >
         <Menu
-          onClick={clickHandler}
-          selectedKeys={[currentSelect]}
-          theme={"light"}
+          className={styles.menu}
+          onSelect={handleMenuSelect}
+          selectedKeys={[currentSelected]}
+          theme="light"
           mode="horizontal"
         >
           <Menu.Item key="timelines">
-            <Icon type="switcher" />
-            动态
+            <Link to="/home">
+              <Icon type="switcher" />
+              动态
+            </Link>
           </Menu.Item>
-          <Menu.Item key="branches">
-            <Icon type="apartment" />
-            部门
+          <Menu.Item key="divisions">
+            <Link to="/home/divisions">
+              <Icon type="apartment" />
+              部门
+            </Link>
           </Menu.Item>
           <Menu.Item key="games">
-            <Icon type="team" />
-            比赛
+            <Link to="/home/contests">
+              <Icon type="team" />
+              比赛
+            </Link>
           </Menu.Item>
         </Menu>
       </Header>
-      <HomepageOptions selected={currentSelect} />
+      <Content>
+        <Switch>
+          <Route
+            exact
+            path="/home"
+            render={() => (
+              <div className={styles.root}>
+                <img className={styles.logo} alt="logo" src={logo} />
+                <Typography.Title level={3}>官网建设中...</Typography.Title>
+              </div>
+            )}
+          />
+          <Route exact path="/home/divisions" component={DivisionPage} />
+          <Route
+            exact
+            path="/home/contests"
+            render={() => (
+              <div className={styles.root}>
+                <img className={styles.logo} alt="logo" src={logo} />
+                <Typography.Title level={3}>官网建设中...</Typography.Title>
+              </div>
+            )}
+          />
+          <Route render={NotFoundPage} />
+        </Switch>
+      </Content>
     </Layout>
   );
 };
