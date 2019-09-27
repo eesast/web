bfa5a27e3a73a1d8beda947636d333fe720a5b2a
Rui Ying <yingrui205@gmail.com>
2019-09-28 04:24:16 +0800

    Update `md2wx` demo

--- src/sites/MarkdownSite.tsx
+++ src/sites/MarkdownSite.tsx
@@ -13,10 +13,51 @@ export interface IMarkdownSiteProps {
 const ApiSite: React.FC<IMarkdownSiteProps> = ({ setSite }) => {
   setSite("others");
 
-  const [text, setText] = useState("");
+  const [text, setText] = useState(`
+# This is md2wx
+
+## It supports \`$\\LaTeX$\`
+
+> with the help of [\`katex\`](https://github.com/KaTeX/KaTeX)
+
+This is \`$a^2$\`; this is \`$b^2$\`.
+
+How about this one:
+
+\`\`\`math
+\\sqrt{a^2+b^2}
+\`\`\`
+
+It can convert \`SVG\` to \`PNG\` so you can copy & paste the HTML to WeChat!
+
+## Of course there is code highlighting
+
+\`\`\`
+const a = 13;
+\`\`\`
+
+---
+
+**Enjoy!**
+
+---
+
+![logo](https://api.eesast.com/static/images/logo.png)
+  `);
   const [highlight, setHighlight] = useState(true);
 
-  const html = useMemo(() => md2wx(text, highlight), [text, highlight]);
+  const html = useMemo(() => md2wx.renderHtml(text, highlight), [
+    text,
+    highlight
+  ]);
+
+  const [pngConverting, setPngConverting] = useState(false);
+
+  const handleConvert = async () => {
+    setPngConverting(true);
+    await md2wx.convertSvgToPng();
+    setPngConverting(false);
+  };
 
   useEffect(() => {
     const clipboard = new Clipboard("#button", {
@@ -40,18 +81,29 @@ const ApiSite: React.FC<IMarkdownSiteProps> = ({ setSite }) => {
           <div
             style={{
               display: "flex",
-              justifyContent: "space-between",
               alignItems: "center",
               marginBottom: 16
             }}
           >
+            <div style={{ flex: 1 }}>
+              若粘贴到图文窗口后图片部分截断或不显示，请单独复制图片到相应位置
+            </div>
             <Switch
               checkedChildren="代码高亮"
               unCheckedChildren="代码高亮"
               checked={highlight}
               onChange={setHighlight}
             />
-            <Button id="button">复制</Button>
+            <Button
+              style={{ marginLeft: 8 }}
+              loading={pngConverting}
+              onClick={handleConvert}
+            >
+              转换 SVG 为 PNG
+            </Button>
+            <Button style={{ marginLeft: 8 }} id="button">
+              复制 HTML 到微信
+            </Button>
           </div>
           <Input.TextArea
             style={{ resize: "none", flex: 1 }}
