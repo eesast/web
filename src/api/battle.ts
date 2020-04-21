0ead56b87ab78ce783650c6a8d9bb6dcb89e388f
Campbell <duskmoon314@users.noreply.github.com>
2020-04-21 19:26:39 +0800

    Add code upload, compile & delete; Add battle start (#343)

--- src/api/battle.ts
+++ src/api/battle.ts
@@ -6,18 +6,15 @@ export const getBattleHistory = async (contestId: number, teamId: number) => {};
 export const startBattle = async (
   contestId: number,
   teams: number[],
-  serverIP: string,
   port: number
 ) => {
   try {
-    const room = await axios.post(`/v1/rooms`, {
+    await axios.post(`/v1/rooms`, {
       contestId: contestId,
       teams: teams,
-      ip: serverIP,
+      ip: "", // ip暂无实际作用
       port: port,
     });
-    const roomId = (room.data as string).split("/").pop();
-    await axios.put(`/v1/rooms/${roomId}/status`, { status: 1 });
   } catch (e) {
     return "Error: Battle not start";
   }
