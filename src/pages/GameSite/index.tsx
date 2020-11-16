2070c1ee99651babcecc6ef1409cb84ac5e96092
dazhuzhu <1787938852@qq.com>
2020-11-16 21:40:52 +0800

    game 的 index.js

---
+++ src/pages/GameSite/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+// import { Layout, Menu } from "antd";
+// import {
+//   SwitcherOutlined,
+//   ApartmentOutlined,
+//   TeamOutlined,
+// } from "@ant-design/icons";
+// import styled from "styled-components";
+// import {
+//   useLocation,
+//   Link,
+//   Switch,
+//   Route,
+//   useRouteMatch,
+// } from "react-router-dom";
+//import NewsPage from "./NewsPage";
+//import DivisionPage from "./DivisionPage";
+//import ContestPage from "./ContestPage";
+//import NotFoundPage from "../NotFoundPage";
+
+// const { Header, Content } = Layout;
+
+// const StyledHeader = styled(Header)`
+//   background-color: white;
+//   display: flex;
+//   flex-direction: row;
+//   height: 48px;
+//   z-index: 98;
+//   border-bottom: 2px #eee solid;
+// `;
+
+// const StyledMenu = styled(Menu)`
+//   &.ant-menu {
+//     line-height: 46px;
+//     border-bottom: unset;
+//   }
+// `;
+const GameSite: React.FC = () => {
+  return <title>我的测试</title>;
+};
+
+export default GameSite;
