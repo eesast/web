7c32297d8c6c945908a20200f7bd59cda765b0be
Dazhuzhu-github <60311880+Dazhuzhu-github@users.noreply.github.com>
2021-01-15 04:08:28 +0800

    增加了router

---
+++ src/pages/ThuaiSite/IntroPage.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Center from "../../components/Center";
+import { Result, Button } from "antd";
+import { Link } from "react-router-dom";
+
+const NotFoundPage = () => {
+  return (
+    <Center>
+      <Result
+        status="404"
+        title="404"
+        subTitle="introPage"
+        extra={
+          <Button type="primary">
+            <Link to="/home"> 返回主页</Link>
+          </Button>
+        }
+      />
+    </Center>
+  );
+};
+
+export default NotFoundPage;
--- src/pages/ThuaiSite/index.tsx
+++ src/pages/ThuaiSite/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-// import {
-//     useLocation,
-//     Link,
-//     Switch,
-//     Route,
-//     useRouteMatch,
-// } from "react-router-dom";
+import {
+  useLocation,
+  Link,
+  Switch,
+  Route,
+  useRouteMatch,
+} from "react-router-dom";
 import {
   //Tabs,
   //Typography,
@@ -13,13 +13,19 @@ import {
   //Row,
   //Col,
   Menu,
+  Layout,
 } from "antd";
-//const { TabPane } = Tabs;
+import RegisterPage from "./RegisterPage";
+import IntroPage from "./IntroPage";
+import NotFoundPage from "../NotFoundPage";
+const { Sider, Content } = Layout;
 const { SubMenu } = Menu;
 const rootSubmenuKeys = ["sub1"];
 const ThuaiSite: React.FC = () => {
-  // const { path, url } = useRouteMatch();
-  // const location = useLocation();
+  const { path, url } = useRouteMatch();
+  const location = useLocation();
+  const page = location.pathname.split("/")[2] ?? "intro";
+
   const [openKeys, setOpenKeys] = React.useState(["sub1"]);
 
   const onOpenChange = (keys: any) => {
@@ -32,21 +38,43 @@ const ThuaiSite: React.FC = () => {
   };
 
   return (
-    <Menu
-      mode="inline"
-      openKeys={openKeys}
-      onOpenChange={onOpenChange}
-      style={{ width: 256 }}
-    >
-      <Menu.Item key="intro">介绍</Menu.Item>
-      <Menu.Item key="source">资源与公告</Menu.Item>
-      <Menu.Item key="register">报名</Menu.Item>
-      <SubMenu key="sub1" title="队伍">
-        <Menu.Item key="join">加入</Menu.Item>
-        <Menu.Item key="manage">管理</Menu.Item>
-      </SubMenu>
-      <Menu.Item key="fight">对战</Menu.Item>
-    </Menu>
+    <Layout>
+      <Sider>
+        <Menu
+          mode="inline"
+          openKeys={openKeys}
+          onOpenChange={onOpenChange}
+          style={{ width: 256 }}
+          selectedKeys={[page]}
+        >
+          <Menu.Item key="intro">
+            <Link to={`${url}/intro`}>介绍</Link>
+          </Menu.Item>
+          <Menu.Item key="source">资源与公告</Menu.Item>
+          <Menu.Item key="register">
+            <Link to={`${url}/register`}>报名</Link>
+          </Menu.Item>
+          <SubMenu key="sub1" title="队伍">
+            <Menu.Item key="join">加入</Menu.Item>
+            <Menu.Item key="manage">管理</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="fight">对战</Menu.Item>
+        </Menu>
+      </Sider>
+      <Content>
+        <Switch>
+          <Route exact path={path}>
+            <IntroPage />
+          </Route>
+          <Route exact path={`${path}/register`}>
+            <RegisterPage />
+          </Route>
+          <Route>
+            <NotFoundPage />
+          </Route>
+        </Switch>
+      </Content>
+    </Layout>
   );
 };
 
