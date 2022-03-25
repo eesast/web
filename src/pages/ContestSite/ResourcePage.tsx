69e0f98685abb025db0ec48cc98feefa897c415a
Roy Chen <84724215+chen-yy20@users.noreply.github.com>
2022-03-25 15:25:04 +0800

    为Contest_page增加了Authroute (#1082)
    * fix: Refactor the GraphQL of  contest_info
    
    重构了比赛公告部分的代码，顺便将底部的2020 EESAST 改成了2022 (
    
    * perf: :zap: 重构graphql代码
    
    除了对战以外的代码基本重构了，另外还修复了一些bug
    
    * add:Modified_updateinfopage
    
    * style:
    
    * pull_the_latest
    
    * feat: :fire: Contest_page增加了Authroute
    
    打开contest_page需要先登录，顺便完善了一些小问题
    
    * fix: 小修改
    
    在MenuPage中混合使用Authroute和Route

--- src/pages/ContestSite/ResourcePage.tsx
+++ src/pages/ContestSite/ResourcePage.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom"
 import {
   Typography,
   Row,
@@ -48,6 +49,7 @@ import type {
 import { getOSS, downloadFile } from "../../helpers/oss";
 import { getUserInfo } from "../../helpers/auth";
 
+
 const { Text } = Typography;
 const { confirm } = Modal;
 
@@ -58,6 +60,8 @@ interface File {
 
 const ResourcePage: React.FC = () => {
   const userInfo = getUserInfo();
+  const location = useLocation()
+  const Contest_id = location.pathname.split("/")[2].replace('}', '')
 
   const {
     data: noticeData,
@@ -66,7 +70,7 @@ const ResourcePage: React.FC = () => {
     refetch: refetchNotices,
   } = useQuery<GetContestNotices, GetContestNoticesVariables>(GET_NOTICES, {
     variables: {
-      contest_id: "3b74b9d3-1955-42d1-954a-ef86b25ca6b7",   // 对应 2021电设 的比赛id
+      contest_id: Contest_id,
     }
   });
 
@@ -132,7 +136,7 @@ const ResourcePage: React.FC = () => {
           title: values.title,
           content: values.content,
           files: JSON.stringify(files),
-          contest_id: "3b74b9d3-1955-42d1-954a-ef86b25ca6b7",
+          contest_id: Contest_id,
         },
       });
     } else {
@@ -141,7 +145,7 @@ const ResourcePage: React.FC = () => {
           title: values.title,
           content: values.content,
           files: JSON.stringify(files),
-          contest_id: "3b74b9d3-1955-42d1-954a-ef86b25ca6b7",
+          contest_id: Contest_id,
         },
       });
     }
@@ -198,7 +202,7 @@ const ResourcePage: React.FC = () => {
             margin-top: 12px;
             margin-right: 24px;
           `}
-          hidden={userInfo?.role !== "counselor" && userInfo?.role !== "root"}
+          hidden={userInfo?.role !== "counselor" && userInfo?.role !== "root" }
           onClick={() => setModalVisible(true)}
         >
           编辑新公告
