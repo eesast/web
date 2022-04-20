2874a0fa0e06e669e829f6222f55226198e83c65
BryantSuen <65460093+BryantSuen@users.noreply.github.com>
2022-04-21 01:47:14 +0800

    feat: :sparkles: add getSharedOss for student to get oss client (#1103)

--- src/helpers/oss.ts
+++ src/helpers/oss.ts
@@ -32,6 +32,33 @@ export const getOSS = async () => {
   }
 };
 
+export const getSharedOSS = async () => {
+  if (
+    oss === null ||
+    expirationTime === null ||
+    expirationTime.getTime() <= new Date().getTime()
+  ) {
+    const response = await axios.get("/static/shared");
+    const auth = response.data;
+    expirationTime = new Date(auth.Expiration);
+
+    const OSS = (await import("ali-oss")).default;
+    oss = new OSS({
+      region: "oss-cn-beijing",
+      accessKeyId: auth.AccessKeyId,
+      accessKeySecret: auth.AccessKeySecret,
+      stsToken: auth.SecurityToken,
+      bucket: "eesast",
+      cname: true,
+      endpoint: process.env.REACT_APP_OSS_URL,
+      secure: true,
+    });
+    return oss;
+  } else {
+    return oss;
+  }
+};
+
 interface File {
   filename: string;
   url: string;
