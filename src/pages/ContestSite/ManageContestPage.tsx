a91838351da7e45bc4506aeb2817ca0166a8032e
wang-zh20 <78086017+wang-zh20@users.noreply.github.com>
2022-04-23 00:13:06 +0800

    Some Fix of BattlePage and Add ContestManagerPage (#1108)
    * Add ContestSite
    
    * Merge branch 'master' of https://github.com/eesast/web into dev
    
    * resolve permission problem of managers
    
    * Resolve problem of deleting contests
    
    Delete contest_team, contest_info, contest_room when deleting contest
    
    * Resolve bugs of JoinPage and ManagePage
    
    resolve bugs of JoinPage and ManagePage, add some part of ManageContestPage
    
    * Resolve fill in class failure and contest_description problem
    
    Change the regular expression of className.
    Remove topic in description by regular expression.
    
    * Update ManageContestPage.tsx
    
    * Add the permission to publish bulletins for the administrator
    
    Add the permission to publish bulletins for the administrator. Continue to improve ManageContestPage. Fix some bugs.
    
    * BattlePage试启用（主要是代码文件的上传与下载） (#1094)
    
    * fix: Refactor the GraphQL of  contest_info
    
    重构了比赛公告部分的代码，顺便将底部的2020 EESAST 改成了2022 (
    
    * perf: :zap: 重构graphql代码
    
    除了对战以外的代码基本重构了，另外还修复了一些bug
    
    * add:Modified_updateinfopage
    
    * style:
    
    * perf: :art: 增加了卡片的进入和退出按钮
    
    * refactor: :zap: 对contest_site的所有功能进行了基于contest_id的修改
    
    基本实现了对于特定比赛的队伍增删改查管理功能
    
    * perf: 只有队长可以移除其他队员
    
    * feat: 增加了部分对战功能，还未完全实现
    
    * pull_the_latest
    
    * feat: :fire: Contest_page增加了Authroute
    
    打开contest_page需要先登录，顺便完善了一些小问题
    
    * fix: 小修改
    
    在MenuPage中混合使用Authroute和Route
    
    * refactor: 重构GQL后重新启用battle_page
    
    * 注释了一些暂时用不上的东西
    
    * timingSwitch
    
    * feat:
    
    * feat: newly_learned_oss
    
    * fucked-up
    
    * feat: :fire: BattlePage试启用
    
    1. 对战暂时只是创建一个room，并未真正对战 2. 天梯得分信息待与后端协调 3. 编译代码和下载编译信息待与后端协调
    
    * 修改了对战页面提示的信息
    
    * 增加Tips
    
    * feat: 🎨 增加了对战记录筛选功能，增加了代码上传时间更新 (#1095)
    
    * fix: Refactor the GraphQL of  contest_info
    
    重构了比赛公告部分的代码，顺便将底部的2020 EESAST 改成了2022 (
    
    * perf: :zap: 重构graphql代码
    
    除了对战以外的代码基本重构了，另外还修复了一些bug
    
    * add:Modified_updateinfopage
    
    * style:
    
    * perf: :art: 增加了卡片的进入和退出按钮
    
    * refactor: :zap: 对contest_site的所有功能进行了基于contest_id的修改
    
    基本实现了对于特定比赛的队伍增删改查管理功能
    
    * perf: 只有队长可以移除其他队员
    
    * feat: 增加了部分对战功能，还未完全实现
    
    * pull_the_latest
    
    * feat: :fire: Contest_page增加了Authroute
    
    打开contest_page需要先登录，顺便完善了一些小问题
    
    * fix: 小修改
    
    在MenuPage中混合使用Authroute和Route
    
    * refactor: 重构GQL后重新启用battle_page
    
    * 注释了一些暂时用不上的东西
    
    * timingSwitch
    
    * feat:
    
    * feat: newly_learned_oss
    
    * fucked-up
    
    * feat: :fire: BattlePage试启用
    
    1. 对战暂时只是创建一个room，并未真正对战 2. 天梯得分信息待与后端协调 3. 编译代码和下载编译信息待与后端协调
    
    * 修改了对战页面提示的信息
    
    * 增加Tips
    
    * feat: :art: 增加了对战记录筛选功能，增加了代码上传时间更新
    
    TODO：1. 代码上传时间可以通过setState改变，但还是需要刷新才能重新彻底渲染出新时间 2. upload会报warning，据说需要升级antd以解决
    
    * Some Fix in BattlePage
    
    Add status and result in the list of battle results
    
    Co-authored-by: BryantSuen <65460093+BryantSuen@users.noreply.github.com>
    Co-authored-by: Roy Chen <84724215+chen-yy20@users.noreply.github.com>

--- src/pages/ContestSite/ManageContestPage.tsx
+++ src/pages/ContestSite/ManageContestPage.tsx
@@ -43,12 +43,16 @@ import {
   DeleteTeamMember,
   DeleteTeamMemberVariables,
 } from "../../api/types";
-import { Button, Card, Form, Input, Layout, List, message, Modal, Result, Row, Table, Typography } from "antd";
+import { Button, Card, Col, Form, Input, Layout, List, message, Modal, Result, Row, Table, Typography, Upload } from "antd";
 import { TableProps } from "antd/lib/table";
-import { ArrowRightOutlined, ExclamationCircleOutlined, MinusCircleOutlined, PlusOutlined, RollbackOutlined } from "@ant-design/icons";
+import { ArrowRightOutlined, DownloadOutlined, ExclamationCircleOutlined, ForwardOutlined, MinusCircleOutlined, PlusOutlined, RollbackOutlined, UploadOutlined } from "@ant-design/icons";
 import TextArea from "antd/lib/input/TextArea";
+import { downloadFile, getSharedOSS } from "../../helpers/oss";
+import OSS from "ali-oss";
+import { RcCustomRequestOptions, RcFile, UploadChangeParam, UploadFile } from "antd/lib/upload/interface";
 
 const { Text } = Typography;
+const { confirm } = Modal;
 
 const ManageContestPage: React.FC = () => {
   //获取比赛ID
@@ -297,6 +301,16 @@ const ListPage: React.FC<{
           >
             添加新队伍
           </Button>
+          <Button
+            css={`
+          margin-top: 12px;
+          margin-left: 15px
+          `}
+            icon={<ForwardOutlined />}
+          >
+            运行比赛
+          </Button>
+
         </Card>
       </Row>
 
@@ -505,6 +519,127 @@ const SubPage: React.FC<{
   }
   //#endregion
 
+  //上传和查看代码界面
+  //列出team已上传的代码文件
+  const [codeList, setCodeList] = useState<OSS.ObjectMeta[]>([]);//返回的查看结果
+  const [codeRoutes1, setCodeRoutes1] = useState<string>();
+  const [codeRoutes2, setCodeRoutes2] = useState<string>();
+  const [codeRoutes3, setCodeRoutes3] = useState<string>();
+  const [codeRoutes4, setCodeRoutes4] = useState<string>();
+  const list = async () => {
+    try {
+      let oss = await getSharedOSS();
+      let result = await oss.list({
+        'max-keys': 5,
+        'prefix': `THUAI5/${props.team_id}/`,
+      },
+        { 'timeout': 0 });
+      console.log(result.objects);
+      setCodeList(result.objects);
+
+      setCodeRoutes1(undefined);
+      setCodeRoutes2(undefined);
+      setCodeRoutes3(undefined);
+      setCodeRoutes4(undefined);
+
+      for (let i = 0; i < result.objects.length; i++) {
+        if (result.objects[i].name === `THUAI5/${props.team_id}/`) {
+          continue;
+        }
+        if (result.objects[i].name.indexOf(`THUAI5/${props.team_id}/`) === 0) {
+          switch (result.objects[i].name.split('/')[2]) {
+            case "player1.cpp": setCodeRoutes1(result.objects[i].name); break;
+            case "player2.cpp": setCodeRoutes2(result.objects[i].name); break;
+            case "player3.cpp": setCodeRoutes3(result.objects[i].name); break;
+            case "player4.cpp": setCodeRoutes4(result.objects[i].name); break;
+          }
+        }
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  useEffect(() => { list() }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  console.log(codeList);
+  console.log(codeRoutes1);
+
+
+
+  const handleUpload = async (e: RcCustomRequestOptions, i: number) => {
+    const oss = await getSharedOSS();
+    const result = await oss.multipartUpload(
+      `THUAI5/${props.team_id}/player${i}.cpp`,
+      e.file,
+      {
+        progress: (progress) =>
+          e.onProgress({ percent: progress * 100 }, e.file),
+      }
+    );
+    if (result.res.status === 200) {
+      e.onSuccess(result.res, e.file);
+    } else {
+      e.onError(new Error());
+    }
+
+  };
+
+  const handleRemove = async (file: UploadFile, i: number) => {
+    if (file.response?.status === 200) {
+      const oss = await getSharedOSS();
+      await oss.delete(`THUAI5/${props.team_id}/player${i}.cpp`);
+    }
+    switch (i) {
+      case 1: setCodeRoutes1(undefined); break;
+      case 2: setCodeRoutes2(undefined); break;
+      case 3: setCodeRoutes3(undefined); break;
+      case 4: setCodeRoutes4(undefined); break;
+    }
+
+  };
+
+  const handleDownload = (file: UploadFile<any>, codeRole: number) => {
+    console.log(file);
+    const codefile = {
+      filename: file.name,
+      url: `/${file.uid}`
+    }
+    message.info("开始下载:" + codefile.filename);
+    downloadFile(codefile).catch(e => {
+      message.error("下载失败");
+    })
+  }
+
+  const handleChange = (info: UploadChangeParam<UploadFile<any>>, codeRole: number) => {
+    console.log(info);
+    console.log(info.file);
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} → P${codeRole} 上传成功`);
+      switch (codeRole) {
+        case 1: setCodeRoutes1(`THUAI5/${props.team_id}/player${codeRole}.cpp`); break;
+        case 2: setCodeRoutes2(`THUAI5/${props.team_id}/player${codeRole}.cpp`); break;
+        case 3: setCodeRoutes3(`THUAI5/${props.team_id}/player${codeRole}.cpp`); break;
+        case 4: setCodeRoutes4(`THUAI5/${props.team_id}/player${codeRole}.cpp`); break;
+      }
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} → P${codeRole} 上传失败`);
+    }
+
+  }
+
+  /* async function getStream() {
+    try {
+      const oss = await getSharedOSS();
+      const result = await oss.get('contest_upload/%E6%B8%B8%E6%88%8F%E8%A7%84%E5%88%99_%E9%80%89%E6%89%8B%E7%89%88.md');
+      console.log(result);
+      console.log(result.content);
+
+    } catch (e) {
+      console.log(e);
+    }
+  } */
+
+
   const contentList = {
     basic: (
       <div
@@ -513,21 +648,21 @@ const SubPage: React.FC<{
           lineHeight: "30px"
         }}
       >
-        <Text>
+        <Text style={{ fontWeight: "700" }}>
           {"队名: "}
         </Text>
         <Text>
           {teamData?.contest_team[0].team_name}
         </Text>
         <br />
-        <Text>
+        <Text style={{ fontWeight: "700" }}>
           {"队长: "}
         </Text>
         <Text>
           {teamData?.contest_team[0].team_leader_id?.name}
         </Text>
         <br />
-        <Text>
+        <Text style={{ fontWeight: "700" }}>
           {"队员: "}
         </Text>
         {
@@ -557,7 +692,7 @@ const SubPage: React.FC<{
             />
         }
 
-        <Text>
+        <Text style={{ fontWeight: "700" }}>
           {"队伍描述: "}
         </Text>
         <br />
@@ -565,7 +700,7 @@ const SubPage: React.FC<{
           {teamData?.contest_team[0].team_intro}
         </Text>
         <br />
-        <Text>
+        <Text style={{ fontWeight: "700" }}>
           {"已提交代码数: "}
         </Text>
         <Text>
@@ -618,8 +753,160 @@ const SubPage: React.FC<{
     ),
     code: (
       <div>
-
-      </div>
+        <Row gutter={16}>
+          <Col span={6}>
+            <Card title="Player1" hoverable>
+              <Upload
+                customRequest={async (e: RcCustomRequestOptions) => {
+                  if (codeRoutes1) {
+                    confirm({
+                      title: "你确定用此代码替换原代码吗？",
+                      icon: <ExclamationCircleOutlined />,
+                      content: '一个角色只能提交一份代码',
+                      onOk() {
+                        handleUpload(e, 1);
+                      }
+                    })
+                  } else {
+                    handleUpload(e, 1);
+                  }
+                }}
+                onRemove={async (file: UploadFile) => {
+                  handleRemove(file, 1);
+                }}
+                onDownload={async (file: UploadFile<any>) => {
+                  handleDownload(file, 1);
+                }}
+                onChange={(info: UploadChangeParam<UploadFile<any>>) => { handleChange(info, 1); }}
+                beforeUpload={(file: RcFile) => {
+                  const isCpp = file.type === 'text/plain' && file.name.endsWith(".cpp");
+                  if (!isCpp) {
+                    message.error(`请上传cpp文件`);
+                  }
+                  return isCpp;
+                }}
+                showUploadList={{
+                  showDownloadIcon: true,
+                  downloadIcon: <DownloadOutlined />,
+                }}
+                fileList={!codeRoutes1 ? undefined : [
+                  {
+                    response: { status: 200 },
+                    status: "done",
+                    uid: codeRoutes1,
+                    name: codeRoutes1?.split('/')[2],
+                    type: "",
+                  } as UploadFile]
+                }
+              >
+                <Button icon={<UploadOutlined />}>上传代码</Button>
+              </Upload>
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Player2" hoverable>
+              <Upload
+                customRequest={async (e: RcCustomRequestOptions) => {
+                  if (codeRoutes2) {
+                    confirm({
+                      title: "你确定用此代码替换原代码吗？",
+                      icon: <ExclamationCircleOutlined />,
+                      content: '一个角色只能提交一份代码',
+                      onOk() {
+                        handleUpload(e, 2);
+                      }
+                    })
+                  } else {
+                    handleUpload(e, 2);
+                  }
+                }}
+                onRemove={async (file: UploadFile) => {
+                  handleRemove(file, 2);
+                }}
+                fileList={!codeRoutes2 ? undefined : [
+                  {
+                    response: { status: 200 },
+                    status: "done",
+                    uid: codeRoutes2,
+                    name: codeRoutes2?.split('/')[2],
+                    type: "",
+                  } as UploadFile]
+                }
+              >
+                <Button icon={<UploadOutlined />}>上传代码</Button>
+              </Upload>
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Player3" hoverable>
+              <Upload
+                customRequest={async (e: RcCustomRequestOptions) => {
+                  if (codeRoutes3) {
+                    confirm({
+                      title: "你确定用此代码替换原代码吗？",
+                      icon: <ExclamationCircleOutlined />,
+                      content: '一个角色只能提交一份代码',
+                      onOk() {
+                        handleUpload(e, 3);
+                      }
+                    })
+                  } else {
+                    handleUpload(e, 3);
+                  }
+                }}
+                onRemove={async (file: UploadFile) => {
+                  handleRemove(file, 3);
+                }}
+                fileList={!codeRoutes3 ? undefined : [
+                  {
+                    response: { status: 200 },
+                    status: "done",
+                    uid: codeRoutes3,
+                    name: codeRoutes3?.split('/')[2],
+                    type: "",
+                  } as UploadFile]
+                }
+              >
+                <Button icon={<UploadOutlined />}>上传代码</Button>
+              </Upload>
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card title="Player4" hoverable>
+              <Upload
+                customRequest={async (e: RcCustomRequestOptions) => {
+                  if (codeRoutes4) {
+                    confirm({
+                      title: "你确定用此代码替换原代码吗？",
+                      icon: <ExclamationCircleOutlined />,
+                      content: '一个角色只能提交一份代码',
+                      onOk() {
+                        handleUpload(e, 4);
+                      }
+                    })
+                  } else {
+                    handleUpload(e, 4);
+                  }
+                }}
+                onRemove={async (file: UploadFile) => {
+                  handleRemove(file, 4);
+                }}
+                fileList={!codeRoutes4 ? undefined : [
+                  {
+                    response: { status: 200 },
+                    status: "done",
+                    uid: codeRoutes4,
+                    name: codeRoutes4?.split('/')[2],
+                    type: "",
+                  } as UploadFile]
+                }
+              >
+                <Button icon={<UploadOutlined />}>上传代码</Button>
+              </Upload>
+            </Card>
+          </Col>
+        </Row>
+      </div >
     ),
     complie: (
       <div>
@@ -635,7 +922,7 @@ const SubPage: React.FC<{
         justify="center"
         css={`margin-top:50px`}>
         <Card
-          style={{ width: "70%" }}
+          style={{ width: "80%" }}
           title={
             <Text css={`
           font-size:xx-large;
