5f115a068f5356a67dbd7b65ccea5e0b9965d8ca
Yichen You <125688164+youyc22@users.noreply.github.com>
2024-03-06 16:43:37 +0800

    fix: 修改了contest_team_member部分的graphql (#1623)
    * use GraphQL CodeGen interface in contestsite
    
    * contest_team_member graphql

--- src/app/ContestSite/CodePage.tsx
+++ src/app/ContestSite/CodePage.tsx
@@ -69,8 +69,8 @@ const CodePage: React.FC<ContestProps> = ({ mode, user }) => {
   });
   const { data: ismemberData } = graphql.useIsTeamMemberSuspenseQuery({
     variables: {
-      _id: user?.uuid!,
-      contest_id: Contest_id,
+      uuid: user?.uuid!,
+      contest_uuid: Contest_id,
     },
   });
 
@@ -561,8 +561,14 @@ const CodePage: React.FC<ContestProps> = ({ mode, user }) => {
         <Col span={2}></Col>
         <Col span={20}>
           <Typography.Text mark>
-            请上传对应角色的<Typography.Text code style={{ color: 'black' }}>AI.cpp</Typography.Text>或
-            <Typography.Text code style={{ color: 'black' }}>AI.py</Typography.Text>
+            请上传对应角色的
+            <Typography.Text code style={{ color: "black" }}>
+              AI.cpp
+            </Typography.Text>
+            或
+            <Typography.Text code style={{ color: "black" }}>
+              AI.py
+            </Typography.Text>
             文件。每次提交任意一份新代码都需要重新编译，即便上传的是Python文件。
           </Typography.Text>
         </Col>
--- src/app/ContestSite/JoinPage.tsx
+++ src/app/ContestSite/JoinPage.tsx
@@ -49,8 +49,8 @@ const JoinPage: React.FC<ContestProps> = ({ mode, user }) => {
   const { data: ismemberData, refetch: refetchismember } =
     graphql.useIsTeamMemberSuspenseQuery({
       variables: {
-        _id: user?.uuid!,
-        contest_id: Contest_id,
+        uuid: user?.uuid!,
+        contest_uuid: Contest_id,
       },
     });
 
@@ -148,8 +148,8 @@ const JoinPage: React.FC<ContestProps> = ({ mode, user }) => {
       try {
         await insertteamMember({
           variables: {
-            team_id: teamId,
-            user_id: user?.uuid!,
+            team_uuid: teamId,
+            user_uuid: user?.uuid!,
           },
         });
         if (!insertError) {
