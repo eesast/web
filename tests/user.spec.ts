c9459581c1065d141322b14c3c6ae1cae5bb87ae
FranGuam <frankguansy@icloud.com>
2024-03-07 17:27:56 +0800

    fix

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -41,35 +41,32 @@ test.describe("Profile", () => {
   });
 
   test("should update department", async ({ page }) => {
-    await page
-      .getByRole("cell", { name: "电子系 edit" })
-      .locator("svg")
-      .click();
-    await page.getByText("电子系").click();
-    await page.getByTitle("医学院").locator("div").click();
-    await page
-      .getByRole("row", { name: "院系 : 医学院 医学院 check close" })
-      .locator("a")
-      .first()
-      .click();
-    await expect(page.getByRole("rowgroup")).toContainText("医学院");
-    await page
-      .getByRole("cell", { name: "医学院 edit" })
-      .locator("svg")
-      .click();
-    await page.getByText("医学院").click();
-    await page.getByTitle("电子系").locator("div").click();
-    await page
-      .getByRole("row", { name: "院系 : 电子系 电子系 check close" })
-      .locator("a")
-      .first()
-      .click();
-    const now = new Date();
-    await expect(page.getByRole("rowgroup")).toContainText(
-      now.getDate().toString(),
-    );
-    await expect(page.getByRole("rowgroup")).toContainText(
-      now.getHours().toString(),
-    );
+    if ((await page.getByRole("rowgroup").innerText()).includes("电子系")) {
+      await page
+        .getByRole("cell", { name: "电子系 edit" })
+        .locator("svg")
+        .click();
+      await page.getByText("电子系").click();
+      await page.getByTitle("医学院").locator("div").click();
+      await page
+        .getByRole("row", { name: "院系 : 医学院 医学院 check close" })
+        .locator("a")
+        .first()
+        .click();
+      await expect(page.getByRole("rowgroup")).toContainText("医学院");
+    } else {
+      await page
+        .getByRole("cell", { name: "医学院 edit" })
+        .locator("svg")
+        .click();
+      await page.getByText("医学院").click();
+      await page.getByTitle("电子系").locator("div").click();
+      await page
+        .getByRole("row", { name: "院系 : 电子系 电子系 check close" })
+        .locator("a")
+        .first()
+        .click();
+      await expect(page.getByRole("rowgroup")).toContainText("电子系");
+    }
   });
 });
