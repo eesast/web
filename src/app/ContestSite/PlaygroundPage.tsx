608775b043f22a71c394cab9ef04a0a271b26c08
youyc22 <youyc22@mails.tsinghua.edu.cn>
2024-04-07 19:24:18 +0800

    revise

--- src/app/ContestSite/PlaygroundPage.tsx
+++ src/app/ContestSite/PlaygroundPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { ContestProps } from ".";
 import { useUrl } from "@/api/hooks/url";
 import * as graphql from "@/generated/graphql";
