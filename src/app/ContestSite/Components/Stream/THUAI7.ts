595aa679b7ee4aeebee2b35e87c7076f18ca6a70
FranGuam <frankguansy@icloud.com>
2024-05-09 03:16:16 +0800

    fix(stream): properly handle quit

---
+++ src/app/ContestSite/Components/Stream/THUAI7.ts
@@ -0,0 +1,49 @@
+import { RpcError } from "grpc-web";
+import { AvailableServiceClient } from "@/generated/grpc-web/THUAI7/ServicesServiceClientPb";
+import * as MessageType from "@/generated/grpc-web/THUAI7/MessageType_pb";
+import * as Message2Clients from "@/generated/grpc-web/THUAI7/Message2Clients_pb";
+import * as Message2Server from "@/generated/grpc-web/THUAI7/Message2Server_pb";
+import { StreamProps } from "../../StreamPage";
+
+const streamTHUAI7: (props: StreamProps) => void = ({
+  streamUrl,
+  port,
+  callback,
+}) => {
+  const client = new AvailableServiceClient(streamUrl + port);
+  const request = new Message2Server.IDMsg();
+  const playerID = Math.floor(Math.random() * 9999) + 2024;
+  request.setPlayerId(playerID);
+  client.tryConnection(
+    request,
+    {},
+    (error: RpcError, response: Message2Clients.BoolRes) => {
+      if (!error) {
+        console.log("Success making gRPC call:", response.toObject());
+        const spectator = new Message2Server.PlayerMsg();
+        spectator.setPlayerId(playerID);
+        const stream = client.addPlayer(spectator, {});
+        stream.on("data", (response) => {
+          if (response.getGameState() === MessageType.GameState.GAME_END) {
+            stream!.cancel();
+            console.log("Game Ended.");
+          }
+          callback(response);
+        });
+        stream.on("status", (status) => {
+          console.log("Received status from server:", status);
+        });
+        stream.on("error", (error) => {
+          console.error("Error making gRPC call:", error);
+        });
+        stream.on("end", () => {
+          console.log("Server ended streaming connection.");
+        });
+      } else {
+        console.error("Error making gRPC call:", error);
+      }
+    },
+  );
+};
+
+export default streamTHUAI7;
--- src/app/ContestSite/StreamPage.tsx
+++ src/app/ContestSite/StreamPage.tsx
@@ -1,35 +1,34 @@
-import { lazy, useEffect } from "react";
+import { useEffect } from "react";
 import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+import ReactRouterPrompt from "react-router-prompt";
+import { FloatButton, Layout, Modal, Progress, Row, message } from "antd";
+import { ArrowsAltOutlined } from "@ant-design/icons";
 import { useUrl } from "../../api/hooks/url";
 import { ContestProps } from ".";
 import * as graphql from "@/generated/graphql";
-import { message } from "antd";
 import NotImplemented from "./Components/NotImplemented";
 import Loading from "../Components/Loading";
-
-const THUAI6 = lazy(() => import("./Components/THUAI6/StreamNative"));
-const THUAI7 = lazy(() => import("./Components/THUAI7/StreamWebGL"));
+import styled from "styled-components";
+import streamTHUAI7 from "./Components/Stream/THUAI7";
 
 export interface StreamProps {
   streamUrl: string;
+  port: string;
+  callback: (response: any) => void;
 }
+
+const Container = styled.div`
+  height: calc(100vh - 72px);
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const StreamPage: React.FC<ContestProps> = ({ mode, user }) => {
   const url = useUrl();
   const contest = url.query.get("contest");
-
-  const { data: contestNameData, error: contestNameError } =
-    graphql.useGetContestNameSuspenseQuery({
-      variables: {
-        contest_id: contest,
-      },
-    });
-  useEffect(() => {
-    if (contestNameError) {
-      message.error("获取比赛信息失败");
-      console.log(contestNameError.message);
-    }
-  });
 
   const { data: contestSwitchData, error: contestSwitchError } =
     graphql.useGetContestSwitchQuery({
@@ -47,18 +46,113 @@ const StreamPage: React.FC<ContestProps> = ({ mode, user }) => {
   const streamUrl = url.query.get("url") ?? "https://live.eesast.com/";
   const port = url.query.get("port") ?? "";
 
-  const Stream = () => {
-    const contestName = contestNameData?.contest_by_pk?.name;
-    if (contestName === "THUAI6") {
-      return <THUAI6 streamUrl={streamUrl + port} />;
-    } else if (contestName === "THUAI7") {
-      return <THUAI7 streamUrl={streamUrl + port} />;
-    } else return <NotImplemented />;
+  const projectUrl =
+    process.env.REACT_APP_STATIC_URL! + "/public/WebGL/THUAI7/";
+  const projectName = "stream";
+
+  const handleCacheControl = (url: string) => {
+    if (url.match(/\.data/) || url.match(/\.wasm/) || url.match(/\.bundle/)) {
+      // 可变的资源
+      return "must-revalidate";
+    }
+    if (url.match(/\.mp4/) || url.match(/\.wav/)) {
+      // 不变的静态资源
+      return "immutable";
+    }
+    return "no-store";
+  };
+
+  const {
+    unityProvider,
+    sendMessage,
+    isLoaded,
+    unload,
+    requestFullscreen,
+    loadingProgression,
+  } = useUnityContext({
+    loaderUrl: projectUrl + projectName + ".loader.js",
+    dataUrl: projectUrl + projectName + ".data",
+    frameworkUrl: projectUrl + projectName + ".framework.js",
+    codeUrl: projectUrl + projectName + ".wasm",
+    streamingAssetsUrl: projectUrl,
+    cacheControl: handleCacheControl,
+  });
+
+  const update = (response: any) => {
+    const message = JSON.stringify(response.toObject());
+    sendMessage("UpdateManager", "UpdateMessageByJson", message);
+    console.log("Received data from server");
+  };
+
+  useEffect(() => {
+    if (!isLoaded) {
+      return;
+    }
+    streamTHUAI7({ streamUrl, port, callback: update });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded]);
+
+  const handleQuit = async () => {
+    try {
+      await unload();
+      return;
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return contestSwitchData ? (
     contestSwitchData?.contest_by_pk?.stream_switch ? (
-      <Stream />
+      <Layout>
+        <Row>
+          {isLoaded === false && (
+            <Container>
+              <Progress
+                type="circle"
+                percent={Math.min(
+                  Math.round(((loadingProgression * 100) / 90) * 99),
+                  100,
+                )}
+              />
+            </Container>
+          )}
+          <Unity
+            unityProvider={unityProvider}
+            css={`
+              width: 100%;
+              max-width: calc((100vh - 72px) / 9 * 16);
+              max-height: calc(100vh - 72px);
+              aspect-ratio: 16 / 9;
+              padding: 0.9vw 1.6vw;
+            `}
+          />
+        </Row>
+        <FloatButton
+          icon={<ArrowsAltOutlined />}
+          style={{ right: 48 }}
+          type="primary"
+          onClick={() => {
+            requestFullscreen(true);
+          }}
+        />
+        <ReactRouterPrompt when={isLoaded}>
+          {({ isActive, onConfirm, onCancel }) => (
+            <Modal
+              open={isActive}
+              cancelText="再看看"
+              centered={true}
+              okText="结束直播"
+              title="离开页面前，请先结束直播"
+              onOk={async () => {
+                await handleQuit();
+                onConfirm();
+              }}
+              onCancel={onCancel}
+              width={320}
+            />
+          )}
+        </ReactRouterPrompt>
+      </Layout>
     ) : (
       <NotImplemented />
     )
