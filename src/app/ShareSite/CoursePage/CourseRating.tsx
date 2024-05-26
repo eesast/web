59eeb736e49cabfad12f792c19fe7386544043b6
Yichen You <125688164+youyc22@users.noreply.github.com>
2024-05-26 23:33:33 +0800

    feat: add courseRating (#1849)
    * add courseRating
    
    * add courseRating

--- src/app/ShareSite/CoursePage/CourseRating.tsx
+++ src/app/ShareSite/CoursePage/CourseRating.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { Button, Drawer, Form, Rate, Space, message } from "antd";
+import React, { useState, useRef, useEffect } from "react";
+import { Button, Card, Drawer, Form, Rate, Space, message, Spin } from "antd";
 import { ReloadOutlined } from "@ant-design/icons";
 import { CourseProps } from ".";
 import * as graphql from "@/generated/graphql";
-import { Radar } from "@ant-design/charts";
+import { Chart } from "@antv/g2";
 
 const CourseRating: React.FC<CourseProps> = ({
   course_uuid,
@@ -13,6 +13,9 @@ const CourseRating: React.FC<CourseProps> = ({
   const [openDrawer, setOpenDrawer] = useState(false);
   const [isRotating, setIsRotating] = useState(false);
   const [form] = Form.useForm();
+  const chartRadarRef = useRef(null);
+  const [loading, setLoading] = useState(true);
+
   const { data: courseRatingData, refetch: refetchCourseRating } =
     graphql.useGetCourseRatingOneQuery({
       variables: {
@@ -21,11 +24,12 @@ const CourseRating: React.FC<CourseProps> = ({
       },
     });
 
-  const { data: courseRatingTotal } = graphql.useGetCourseRatingQuery({
-    variables: {
-      course_uuid: course_uuid,
-    },
-  });
+  const { data: courseRatingTotal, refetch: refetchCourseRatingTotal } =
+    graphql.useGetCourseRatingQuery({
+      variables: {
+        course_uuid: course_uuid,
+      },
+    });
 
   const [addCourseRating] = graphql.useAddCourseRatingMutation();
   const [updateCourseRating] = graphql.useUpdateCourseRatingMutation();
@@ -34,6 +38,7 @@ const CourseRating: React.FC<CourseProps> = ({
   const showDrawer = () => {
     handleGetCourseRating();
     setOpenDrawer(true);
+    setLoading(true);
   };
   const closeDrawer = () => {
     setOpenDrawer(false);
@@ -101,55 +106,115 @@ const CourseRating: React.FC<CourseProps> = ({
         message.success("评分已添加");
       }
       refetchCourseRating();
+      refetchCourseRatingTotal();
     } catch (error) {
       message.error("保存评分失败");
       console.log(error);
     }
   };
 
-  const radarData = [
-    {
-      item: "任务量",
-      score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim1,
-    },
-    {
-      item: "内容难度",
-      score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim2,
-    },
-    {
-      item: "上课质量",
-      score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim3,
-    },
-    {
-      item: "收获感",
-      score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim4,
-    },
-    {
-      item: "给分好坏",
-      score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim5,
-    },
-    {
-      item: "考试作业讲课相关度",
-      score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim6,
-    },
-  ];
-
-  const config = {
-    tutle: {
-      visable: true,
-      text: "课程评分雷达图",
-    },
-    radarData,
-    angleField: "item",
-    radiusField: "score",
-    radiusAxis: {
-      grid: {
-        alternateColor: ["rgba(0, 0, 0, 0.04)", null],
+  console.log(courseRatingData);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    if (!chartRadarRef.current) return;
+    const radarData = [
+      {
+        item: "任务量",
+        score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim1,
       },
-    },
-    area: { visible: false },
-    point: { visible: true },
-  };
+      {
+        item: "内容难度",
+        score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim2,
+      },
+      {
+        item: "上课质量",
+        score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim3,
+      },
+      {
+        item: "收获感",
+        score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim4,
+      },
+      {
+        item: "给分好坏",
+        score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim5,
+      },
+      {
+        item: "考试作业讲课相关度",
+        score: courseRatingTotal?.course_rating_aggregate?.aggregate?.avg?.dim6,
+      },
+    ];
+
+    const chart = new Chart({
+      container: chartRadarRef.current,
+      autoFit: true,
+    });
+
+    chart.options({
+      type: "view",
+      autoFit: true,
+      data: radarData,
+      scale: {
+        x: { padding: 0.5, align: 0 },
+        y: { tickCount: 1, domainMin: 0, domainMax: 5 },
+      },
+      coordinate: { type: "polar" },
+      axis: {
+        x: {
+          grid: true,
+          gridLineWidth: 2,
+          gridSroke: mode === "dark" ? "white" : "black",
+          tick: false,
+          gridLineDash: [0, 0],
+          labelFontSize: 12,
+          labelFill: mode === "dark" ? "white" : "black",
+          labelAlign: "horizontal",
+          labelFontWeight: "bold",
+          labelSpacing: 15,
+        },
+        y: {
+          zIndex: 1,
+          title: false,
+          gridLineWidth: 2,
+          gridSroke: mode === "dark" ? "white" : "black",
+          labelFill: mode === "dark" ? "white" : "black",
+          labelFontSize: 12,
+          gridLineDash: [0, 0],
+        },
+      },
+      interaction: { tooltip: { crosshairsLineDash: [4, 4] } },
+      children: [
+        {
+          type: "area",
+          encode: { x: "item", y: "score", color: "#4083FE" },
+          style: { fillOpacity: 0.5 },
+        },
+        {
+          type: "line",
+          encode: { x: "item", y: "score", color: "#4083FE" },
+          style: { lineWidth: 2 },
+        },
+        {
+          type: "point",
+          encode: {
+            x: "item",
+            y: "score",
+            color: "#4184FE",
+            shape: "point",
+            size: 3,
+          },
+          tooltip: null,
+        },
+      ],
+    });
+
+    chart.render();
+
+    setLoading(false);
+    return () => {
+      chart.destroy();
+    };
+  });
+
   return (
     <>
       <Button
@@ -171,9 +236,10 @@ const CourseRating: React.FC<CourseProps> = ({
               alignItems: "center",
             }}
           >
-            <span>课程评分</span>
+            <span style={{ fontSize: "24px" }}>课程评分</span>
             <Space>
               <Button
+                size="large"
                 type="link"
                 icon={
                   <ReloadOutlined
@@ -190,7 +256,7 @@ const CourseRating: React.FC<CourseProps> = ({
             </Space>
           </div>
         }
-        width={520}
+        width={600}
         onClose={closeDrawer}
         open={openDrawer}
         key="course_rating"
@@ -200,47 +266,112 @@ const CourseRating: React.FC<CourseProps> = ({
               textAlign: "right",
             }}
           >
-            <Button onClick={closeDrawer} style={{ marginRight: 8 }}>
+            <Button
+              size="large"
+              onClick={closeDrawer}
+              style={{ width: 80, height: 40, marginRight: 12 }}
+            >
               取消
             </Button>
-            <Button onClick={handleSaveRating} type="primary">
+            <Button
+              onClick={handleSaveRating}
+              style={{ width: 80, height: 40, marginRight: 12 }}
+              type="primary"
+            >
               保存
             </Button>
           </div>
         }
       >
-        <Radar {...config} />
-        <Form
-          layout="vertical"
-          form={form}
-          initialValues={{
-            dim1: 0,
-            dim2: 0,
-            dim3: 0,
-            dim4: 0,
-            dim5: 0,
-            dim6: 0,
-          }}
+        <Card
+          hoverable
+          title={
+            <span style={{ fontSize: "20px", fontWeight: "bold" }}>
+              平均评分
+            </span>
+          }
         >
-          <Form.Item name="dim1" label="任务量">
-            <Rate style={{ fontSize: 28 }} />
-          </Form.Item>
-          <Form.Item name="dim2" label="内容难度">
-            <Rate style={{ fontSize: 28 }} />
-          </Form.Item>
-          <Form.Item name="dim3" label="上课质量">
-            <Rate style={{ fontSize: 28 }} />
-          </Form.Item>
-          <Form.Item name="dim4" label="收获感">
-            <Rate style={{ fontSize: 28 }} />
-          </Form.Item>
-          <Form.Item name="dim5" label="给分好坏">
-            <Rate style={{ fontSize: 28 }} />
-          </Form.Item>
-          <Form.Item name="dim6" label="考试作业讲课相关度">
-            <Rate style={{ fontSize: 28 }} />
-          </Form.Item>
-        </Form>
+          <Spin spinning={loading}>
+            <div
+              ref={chartRadarRef}
+              style={{ width: "100%", height: "38vh" }}
+            />
+          </Spin>
+        </Card>
+        <br />
+        <Card
+          hoverable
+          title={
+            <span style={{ fontSize: "20px", fontWeight: "bold" }}>
+              我的评分
+            </span>
+          }
+          style={{ fontSize: "30px" }}
+        >
+          <Form
+            layout="inline"
+            labelAlign="left"
+            labelCol={{ span: 12 }}
+            wrapperCol={{ span: 22 }}
+            style={{
+              marginLeft: "10px",
+            }}
+            form={form}
+            initialValues={{
+              dim1: 0,
+              dim2: 0,
+              dim3: 0,
+              dim4: 0,
+              dim5: 0,
+              dim6: 0,
+            }}
+          >
+            <Form.Item
+              name="dim1"
+              label={<span style={{ paddingLeft: "84px" }}>任务量</span>}
+              style={{ marginBottom: "18px" }}
+            >
+              <Rate style={{ fontSize: 35 }} />
+            </Form.Item>
+            <Form.Item
+              name="dim2"
+              label={<span style={{ paddingLeft: "70px" }}>内容难度</span>}
+              style={{ marginBottom: "18px" }}
+            >
+              <Rate style={{ fontSize: 35 }} />
+            </Form.Item>
+            <Form.Item
+              name="dim3"
+              label={<span style={{ paddingLeft: "70px" }}>上课质量</span>}
+              style={{ marginBottom: "18px" }}
+            >
+              <Rate style={{ fontSize: 35 }} />
+            </Form.Item>
+            <Form.Item
+              name="dim4"
+              label={<span style={{ paddingLeft: "84px" }}>收获感</span>}
+              style={{ marginBottom: "18px" }}
+            >
+              <Rate style={{ fontSize: 35 }} />
+            </Form.Item>
+            <Form.Item
+              name="dim5"
+              label={<span style={{ paddingLeft: "70px" }}>给分好坏</span>}
+              style={{ marginBottom: "18px" }}
+            >
+              <Rate style={{ fontSize: 35 }} />
+            </Form.Item>
+            <Form.Item
+              name="dim6"
+              label={
+                <span style={{ paddingLeft: "0px" }}>考试作业讲课相关度</span>
+              }
+              style={{ marginBottom: "18px" }}
+            >
+              <Rate style={{ fontSize: 35 }} />
+            </Form.Item>
+          </Form>
+        </Card>
       </Drawer>
     </>
   );
